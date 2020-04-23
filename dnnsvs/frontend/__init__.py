3dd3eebd424462dc4291a907a942eebfcc76bdc4
Ryuichi Yamamoto <zryuichi@gmail.com>
2020-04-24 00:43:32 +0900

    Add frontend and dsp modules (no content at the moment)

---
+++ dnnsvs/frontend/__init__.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+
+"""Language-dependent frontend processing
+
+which converts muxicxml or MIDI file to context feature vectors.
+"""
+
+# TODO: Add interface definition
---
+++ dnnsvs/frontend/ja.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+
+try:
+    import pysinsy
+except ImportError as e:
+    print("Pysinsy must be manually installed!")
+    raise e
+
+# TODO: consider replacing pysinsy to pure python implementation
