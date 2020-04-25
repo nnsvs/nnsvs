a4d3f44634390569d62fb68ae2053410924875ad
Ryuichi Yamamoto <zryuichi@gmail.com>
2020-04-25 21:31:19 +0900

    Add xml2lab function for ja

--- dnnsvs/frontend/ja.py
+++ dnnsvs/frontend/ja.py
@@ -1,5 +1,9 @@
 # coding: utf-8
 
+import os
+from nnmnkwii.io import hts
+
+
 try:
     import pysinsy
 except ImportError as e:
@@ -7,3 +11,32 @@ except ImportError as e:
     raise e
 
 # TODO: consider replacing pysinsy to pure python implementation
+
+_global_sinsy = None
+
+
+def _lazy_init():
+    global _global_sinsy
+    if _global_sinsy is None:
+        _global_sinsy = pysinsy.sinsy.Sinsy()
+        # TODO: remove hardcode
+        assert _global_sinsy.setLanguages("j", "/usr/local/lib/sinsy/dic")
+
+
+def xml2lab(xml):
+    """Convert musicxml to HTS full context labels
+
+    Args:
+        xml (str): Path to musicxml file.
+
+    Returns:
+        HTS full context labels
+    """
+    _lazy_init()
+    global _global_sinsy
+
+    _global_sinsy.loadScoreFromMusicXML(xml)
+    label = _global_sinsy.createLabelData(False, 1, 1)
+    label = hts.load(lines=label.getData())
+    _global_sinsy.clearScore()
+    return label
