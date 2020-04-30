1d56d74ee1e83c14609458e4cee4cdb823512433
Ryuichi Yamamoto <zryuichi@gmail.com>
2020-04-30 22:18:47 +0900

    Add empty base module

---
+++ dnnsvs/base.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+
+import torch
+from torch import nn
+from torch.nn import functional as F
+from abc import ABC, abstractmethod
+
+
+class TimeLagModel(ABC):
+    @abstractmethod
+    def forward(self, feats, feats_lens=None):
+        pass
+
+class DurationModel(ABC):
+    @abstractmethod
+    def forward(self, feats, feats_lens=None):
+        pass
+
+
+class AcousticModel(ABC):
+    @abstractmethod
+    def forward(self, feats, feats_lens=None):
+        pass
