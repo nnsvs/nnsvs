91bed0b050dbdc82729a92eb914e16de1c622a91
Ryuichi Yamamoto <zryuichi@gmail.com>
2020-05-02 00:19:19 +0900

    Add io module

---
+++ dnnsvs/io/hts.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+def get_note_indices(lab):
+    note_indices = [0]
+    last_start_time = lab.start_times[0]
+    for idx in range(1, len(lab)):
+        if lab.start_times[idx] != last_start_time:
+            note_indices.append(idx)
+            last_start_time = lab.start_times[idx]
+        else:
+            pass
+    return note_indices
