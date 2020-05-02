de3fbeb4ae47fa9ee71fbd4463a1b011ff806d08
Ryuichi Yamamoto <zryuichi@gmail.com>
2020-05-02 21:38:51 +0900

    BUG fix: checkpoint was not loaded, which caused random results :(
    fixes predict.py to include MLPG if necessary

--- dnnsvs/bin/predict.py
+++ dnnsvs/bin/predict.py
@@ -4,17 +4,19 @@ import hydra
 from hydra.utils import to_absolute_path
 from omegaconf import DictConfig, OmegaConf
 import numpy as np
-from glob import glob
+import joblib
 from tqdm import tqdm
 from os.path import basename, splitext, exists, join
 import os
 import torch
 from torch import nn
 from torch.nn import functional as F
-from torch import optim
-from nnmnkwii.datasets import FileDataSource, FileSourceDataset
-from dnnsvs.logger import getLogger
+from nnmnkwii.datasets import FileSourceDataset
+
+from dnnsvs.gen import get_windows
+from dnnsvs.multistream import multi_stream_mlpg
 from dnnsvs.bin.train import NpyFileSource
+from dnnsvs.logger import getLogger
 
 logger = None
 
@@ -32,18 +34,32 @@ def my_app(config : DictConfig) -> None:
     out_dir = to_absolute_path(config.out_dir)
     os.makedirs(out_dir, exist_ok=True)
 
-    model_def = OmegaConf.load(to_absolute_path(config.model.model_yaml))
-    model = hydra.utils.instantiate(model_def).to(device)
+    model_config = OmegaConf.load(to_absolute_path(config.model.model_yaml))
+    model = hydra.utils.instantiate(model_config.netG).to(device)
+    checkpoint = torch.load(to_absolute_path(config.model.checkpoint))
+    model.load_state_dict(checkpoint["state_dict"])
+
+    scaler = joblib.load(to_absolute_path(config.out_scaler_path))
 
     in_feats = FileSourceDataset(NpyFileSource(in_dir))
 
     with torch.no_grad():
         for idx in tqdm(range(len(in_feats))):
-            feats = torch.from_numpy(in_feats[idx]).unsqueeze(0)
-            out = model(feats, [feats.shape[1]]).squeeze(0)
+            feats = torch.from_numpy(in_feats[idx]).unsqueeze(0).to(device)
+            out = model(feats, [feats.shape[1]]).squeeze(0).cpu().data.numpy()
+
+            out = scaler.inverse_transform(out)
+
+            # Apply MLPG if necessary
+            if np.any(model_config.has_dynamic_features):
+                windows = get_windows(3)
+                out = multi_stream_mlpg(
+                    out, scaler.var_, windows, model_config.stream_sizes,
+                    model_config.has_dynamic_features)
+
             name = basename(in_feats.collected_files[idx][0])
             out_path = join(out_dir, name)
-            np.save(out_path, out.data.numpy(), allow_pickle=False)
+            np.save(out_path, out, allow_pickle=False)
 
 
 def entry():
