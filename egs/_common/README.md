50ec40739353453a3c99e3b1921ba085a07c2f82
Ryuichi Yamamoto <zryuichi@gmail.com>
2020-10-31 20:59:47 +0900

    Recipe refactors
    - made common scripts and removed the most of duplicated scripts
    - put the common hed file to egs/_common/hed
    - renamed utils to local following kaldi

---
+++ egs/_common/README.md
@@ -0,0 +1,6 @@
+# Common
+
+Common scripts and resources
+
+- spsvs: scripts for stastistical parametric singing voice synthesis (SPSVS) recipes.
+- hed: HTS-style question files
---
+++ egs/_common/spsvs/feature_generation.sh
@@ -0,0 +1,39 @@
+# NOTE: the script is supposed to be used called from nnsvs recipes.
+# Please don't try to run the shell script directry.
+
+for s in ${datasets[@]};
+do
+    nnsvs-prepare-features utt_list=data/list/$s.list out_dir=$dump_org_dir/$s/  \
+    question_path=$question_path
+done
+
+# Compute normalization stats for each input/output
+mkdir -p $dump_norm_dir
+for inout in "in" "out"; do
+    if [ $inout = "in" ]; then
+        scaler_class="sklearn.preprocessing.MinMaxScaler"
+    else
+        scaler_class="sklearn.preprocessing.StandardScaler"
+    fi
+    for typ in timelag duration acoustic;
+    do
+        find $dump_org_dir/$train_set/${inout}_${typ} -name "*feats.npy" > train_list.txt
+        scaler_path=$dump_org_dir/${inout}_${typ}_scaler.joblib
+        nnsvs-fit-scaler list_path=train_list.txt scaler.class=$scaler_class \
+            out_path=$scaler_path
+        rm -f train_list.txt
+        cp -v $scaler_path $dump_norm_dir/${inout}_${typ}_scaler.joblib
+    done
+done
+
+# apply normalization
+for s in ${datasets[@]}; do
+    for inout in "in" "out"; do
+        for typ in timelag duration acoustic;
+        do
+            nnsvs-preprocess-normalize in_dir=$dump_org_dir/$s/${inout}_${typ}/ \
+                scaler_path=$dump_org_dir/${inout}_${typ}_scaler.joblib \
+                out_dir=$dump_norm_dir/$s/${inout}_${typ}/
+        done
+    done
+done
