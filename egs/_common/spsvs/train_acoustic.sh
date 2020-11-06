b2a4defc8d3d3710880a9b750cf65bb109b06168
Ryuichi Yamamoto <zryuichi@gmail.com>
2020-11-06 21:50:25 +0900

    egs: revise comon shell scripts for more flexibility

--- egs/_common/spsvs/train_acoustic.sh
+++ egs/_common/spsvs/train_acoustic.sh
@@ -1,12 +1,18 @@
 # NOTE: the script is supposed to be used called from nnsvs recipes.
 # Please don't try to run the shell script directry.
 
+if [ -d conf/train ]; then
+    ext="--config-dir conf/train"
+else
+    ext=""
+fi
+
 if [ ! -z "${pretrained_expdir}" ]; then
     resume_checkpoint=$pretrained_expdir/acoustic/latest.pth
 else
     resume_checkpoint=
 fi
-xrun nnsvs-train --config-dir conf/train \
+xrun nnsvs-train $ext \
     data.train_no_dev.in_dir=$dump_norm_dir/$train_set/in_acoustic/ \
     data.train_no_dev.out_dir=$dump_norm_dir/$train_set/out_acoustic/ \
     data.dev.in_dir=$dump_norm_dir/$dev_set/in_acoustic/ \
