e5f1fa3ab8ce983f5bbf8701100059a2d3fad792
Ryuichi Yamamoto <zryuichi@gmail.com>
2022-03-11 00:02:16 +0900

    Add release.sh

---
+++ release.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Script for Pypi release
+# 0. Make sure you are on git tag
+# 1. Run the script
+# 2. Upload sdist
+
+set -e
+
+script_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
+cd $script_dir
+
+TAG=$(git describe --exact-match --tags HEAD)
+
+VERSION=${TAG/v/}
+
+python setup.py develop sdist
+echo "*** Ready to release! nnsvs $TAG ***"
+echo "Please make sure that release verion is correct."
+cat nnsvs/version.py
+echo "Please run the following command manually:"
+echo twine upload dist/nnsvs-${VERSION}.tar.gz --repository-url https://upload.pypi.org/legacy/
