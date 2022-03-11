e1451e1b72f0f8ab66a352be4bdcdb495d5d639e
Ryuichi Yamamoto <zryuichi@gmail.com>
2022-03-11 20:58:26 +0900

    Add synthesis script for resf0

---
+++ egs/_common/spsvs/synthesis_resf0.sh
@@ -0,0 +1,50 @@
+# NOTE: the script is supposed to be used called from nnsvs recipes.
+# Please don't try to run the shell script directory.
+
+if [ -d conf/synthesis ]; then
+    ext="--config-dir conf/synthesis"
+else
+    ext=""
+fi
+
+if [ -z $timelag_eval_checkpoint ]; then
+    timelag_eval_checkpoint=best_loss.pth
+fi
+if [ -z $duration_eval_checkpoint ]; then
+    duration_eval_checkpoint=best_loss.pth
+fi
+if [ -z $acoustic_eval_checkpoint ]; then
+    acoustic_eval_checkpoint=latest.pth
+fi
+
+for s in ${testsets[@]}; do
+    for input in label_phone_score label_phone_align; do
+        if [ $input = label_phone_score ]; then
+            ground_truth_duration=false
+        else
+            ground_truth_duration=true
+        fi
+        xrun nnsvs-synthesis $ext \
+            question_path=$question_path \
+            log_f0_conditioning=true \ # must be set to true for residual f0 modeling
+            timelag=$timelag_synthesis \
+            duration=$duration_synthesis \
+            acoustic=$acoustic_synthesis \
+            timelag.checkpoint=$expdir/timelag/$timelag_eval_checkpoint \
+            timelag.in_scaler_path=$dump_norm_dir/in_timelag_scaler.joblib \
+            timelag.out_scaler_path=$dump_norm_dir/out_timelag_scaler.joblib \
+            timelag.model_yaml=$expdir/timelag/model.yaml \
+            duration.checkpoint=$expdir/duration/$duration_eval_checkpoint \
+            duration.in_scaler_path=$dump_norm_dir/in_duration_scaler.joblib \
+            duration.out_scaler_path=$dump_norm_dir/out_duration_scaler.joblib \
+            duration.model_yaml=$expdir/duration/model.yaml \
+            acoustic.checkpoint=$expdir/acoustic/$acoustic_eval_checkpoint \
+            acoustic.in_scaler_path=$dump_norm_dir/in_acoustic_scaler.joblib \
+            acoustic.out_scaler_path=$dump_norm_dir/out_acoustic_scaler.joblib \
+            acoustic.model_yaml=$expdir/acoustic/model.yaml \
+            utt_list=./data/list/$s.list \
+            in_dir=data/acoustic/$input/ \
+            out_dir=$expdir/synthesis/$s/${acoustic_eval_checkpoint/.pth/}/$input \
+            ground_truth_duration=$ground_truth_duration
+    done
+done
