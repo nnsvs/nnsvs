a1f0f3c3c7dee86e047aaccf4ee8b6ded96ed56f
nicolalandro <nicolaxx94@Live.it>
2022-03-22 18:32:11 +0100

    add venv and use pysen

--- streamlit_demo/app.py
+++ streamlit_demo/app.py
@@ -1,11 +1,10 @@
 import os
-import streamlit as st
-import soundfile as sf
-
-import numpy as np
 
 import librosa
+import numpy as np
 import pysinsy
+import soundfile as sf
+import streamlit as st
 from nnmnkwii.io import hts
 from nnsvs.svs import SPSVS
 
@@ -13,39 +12,35 @@ st.title("NNSVS Demo")
 st.markdown("Upload your .xml music file with text as input to make it singed.")
 
 models = {
-  'kiritan': '20220321_kiritan_timelag_mdn_duration_mdn_acoustic_resf0conv',
-  'yoko' : '20220322_yoko_timelag_mdn_duration_mdn_acoustic_resf0conv'
+    "kiritan": "20220321_kiritan_timelag_mdn_duration_mdn_acoustic_resf0conv",
+    "yoko": "20220322_yoko_timelag_mdn_duration_mdn_acoustic_resf0conv",
 }
 
-voice_option = st.selectbox(
-     'Select the voice',
-     models.keys()
-)
-uploaded_file = st.file_uploader("Choose a .xml music file", type='xml')
+voice_option = st.selectbox("Select the voice", models.keys())
+uploaded_file = st.file_uploader("Choose a .xml music file", type="xml")
 
 if uploaded_file:
-  with st.spinner(f"Sinthezizing to wav"):
-    # save input file
-    with open(os.path.join(os.getcwd(), uploaded_file.name), "wb") as f:
-      f.write(uploaded_file.getbuffer())
-    file_path = os.path.join(os.getcwd(), uploaded_file.name)
+    with st.spinner(f"Sinthezizing to wav"):
+        # save input file
+        with open(os.path.join(os.getcwd(), uploaded_file.name), "wb") as f:
+            f.write(uploaded_file.getbuffer())
+        file_path = os.path.join(os.getcwd(), uploaded_file.name)
 
-    # synthesize
-    model_dir = models[voice_option]
-    contexts = pysinsy.extract_fullcontext(file_path)
-    labels = hts.HTSLabelFile.create_from_contexts(contexts)
+        # synthesize
+        model_dir = models[voice_option]
+        contexts = pysinsy.extract_fullcontext(file_path)
+        labels = hts.HTSLabelFile.create_from_contexts(contexts)
 
-    engine = SPSVS(model_dir)
-    wav, sr = engine.svs(labels)
+        engine = SPSVS(model_dir)
+        wav, sr = engine.svs(labels)
 
-    wav = librosa.effects.trim(wav.astype(np.float64), top_db=40)[0]
-    
-    # save wav
-    out_path = file_path + '.wav'
-    sf.write(out_path, wav, sr)
+        wav = librosa.effects.trim(wav.astype(np.float64), top_db=40)[0]
 
-    # show audio player
-    with open(out_path, 'rb') as audio_file:
-      audio_bytes = audio_file.read()
-    st.audio(audio_bytes)
+        # save wav
+        out_path = file_path + ".wav"
+        sf.write(out_path, wav, sr)
 
+        # show audio player
+        with open(out_path, "rb") as audio_file:
+            audio_bytes = audio_file.read()
+        st.audio(audio_bytes)
