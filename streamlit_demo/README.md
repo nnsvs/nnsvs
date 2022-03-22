376c3511008499403ad0bfabbec096863341fc4f
nicolalandro <nicolaxx94@Live.it>
2022-03-22 19:55:09 +0100

    stop to save files, correct typo errors, automatic download models and correct audio problems

--- streamlit_demo/README.md
+++ streamlit_demo/README.md
@@ -9,16 +9,6 @@ pip install streamlit git+https://github.com/r9y9/nnsvs
 # or if you must specify the python version
 python3.8 -m pip install streamlit git+https://github.com/r9y9/nnsvs
 ```
-* download the trained models
-```
-[ ! -e 20220321_kiritan_timelag_mdn_duration_mdn_acoustic_resf0conv ] && curl -q -LO https://www.dropbox.com/s/87rqto5l5rpav2n/20220321_kiritan_timelag_mdn_duration_mdn_acoustic_resf0conv.zip
-unzip -qq -o 20220321_kiritan_timelag_mdn_duration_mdn_acoustic_resf0conv.zip
-
-model_dir = "20220322_yoko_timelag_mdn_duration_mdn_acoustic_resf0conv"
-
-! [ ! -e 20220322_yoko_timelag_mdn_duration_mdn_acoustic_resf0conv ] && curl -q -LO https://www.dropbox.com/s/l1wo9dewfuk3s1v/20220322_yoko_timelag_mdn_duration_mdn_acoustic_resf0conv.zip
-! unzip -qq -o 20220322_yoko_timelag_mdn_duration_mdn_acoustic_resf0conv.zip
-```
 * run streaimlit app
 ```
 streamlit run streamlit_demo/app.py
--- streamlit_demo/app.py
+++ streamlit_demo/app.py
@@ -1,4 +1,4 @@
-import os
+import tempfile
 
 import librosa
 import numpy as np
@@ -6,41 +6,35 @@ import pysinsy
 import soundfile as sf
 import streamlit as st
 from nnmnkwii.io import hts
+from nnsvs.pretrained import create_svs_engine
 from nnsvs.svs import SPSVS
 
 st.title("NNSVS Demo")
-st.markdown("Upload your .xml music file with text as input to make it singed.")
+st.markdown("Upload your .xml music file with text as input to make it sing.")
 
 models = {
-    "kiritan": "20220321_kiritan_timelag_mdn_duration_mdn_acoustic_resf0conv",
-    "yoko": "20220322_yoko_timelag_mdn_duration_mdn_acoustic_resf0conv",
+    "kiritan": "r9y9/20220321_kiritan_timelag_mdn_duration_mdn_acoustic_resf0conv",
+    "yoko": "r9y9/20220322_yoko_timelag_mdn_duration_mdn_acoustic_resf0conv",
 }
 
 voice_option = st.selectbox("Select the voice", models.keys())
 uploaded_file = st.file_uploader("Choose a .xml music file", type="xml")
 
-if uploaded_file:
-    with st.spinner(f"Sinthezizing to wav"):
-        # save input file
-        with open(os.path.join(os.getcwd(), uploaded_file.name), "wb") as f:
-            f.write(uploaded_file.getbuffer())
-        file_path = os.path.join(os.getcwd(), uploaded_file.name)
-
+if st.button("synthesis") and uploaded_file:
+    with st.spinner(f"Synthesizing to wav"):
         # synthesize
-        model_dir = models[voice_option]
-        contexts = pysinsy.extract_fullcontext(file_path)
+        contexts = pysinsy.extract_fullcontext(uploaded_file.name)
         labels = hts.HTSLabelFile.create_from_contexts(contexts)
 
-        engine = SPSVS(model_dir)
+        engine = create_svs_engine(models[voice_option])
         wav, sr = engine.svs(labels)
 
-        wav = librosa.effects.trim(wav.astype(np.float64), top_db=40)[0]
-
-        # save wav
-        out_path = file_path + ".wav"
-        sf.write(out_path, wav, sr)
+        wav = librosa.effects.trim(wav.astype(np.float64), top_db=40)[0].astype(
+            np.int16
+        )
 
         # show audio player
-        with open(out_path, "rb") as audio_file:
-            audio_bytes = audio_file.read()
-        st.audio(audio_bytes)
+        with tempfile.NamedTemporaryFile(suffix=".wav") as f:
+            sf.write(f.name, wav, sr)
+            with open(f.name, "rb") as wav_file:
+                st.audio(wav_file.read(), format="audio/wav")
