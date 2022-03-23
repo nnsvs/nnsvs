68c48dadcd21aff728f4a24086ad51c771b9abcf
nicolalandro <nicolaxx94@Live.it>
2022-03-23 20:18:41 +0100

    add partiture print

--- streamlit_demo/app.py
+++ streamlit_demo/app.py
@@ -1,11 +1,13 @@
 import tempfile
 
+import music21
 import numpy as np
 import pysinsy
 import soundfile as sf
 import streamlit as st
 from nnmnkwii.io import hts
 from nnsvs.pretrained import create_svs_engine
+from PIL import Image
 
 st.title("NNSVS Demo")
 st.markdown("Upload your .xml music file with text as input to make it sing.")
@@ -20,16 +22,20 @@ uploaded_file = st.file_uploader("Choose a .xml music file", type="xml")
 
 if st.button("synthesis") and uploaded_file:
     with st.spinner("Synthesizing to wav"):
-        # synthesize
         with tempfile.NamedTemporaryFile(suffix=".xml") as f:
             f.write(uploaded_file.getbuffer())
+
+            music = music21.converter.parse(f.name)
+            streaming_partiture = str(music.write("lily.png"))
+            image = Image.open(streaming_partiture)
+            st.image(image)
+
             contexts = pysinsy.extract_fullcontext(f.name)
         labels = hts.HTSLabelFile.create_from_contexts(contexts)
 
         engine = create_svs_engine(models[voice_option])
         wav, sr = engine.svs(labels)
 
-        # show audio player
         with tempfile.NamedTemporaryFile(suffix=".wav") as f:
             sf.write(f.name, wav.astype(np.int16), sr)
             with open(f.name, "rb") as wav_file:
