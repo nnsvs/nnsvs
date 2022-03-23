16b7ec88ddea93a38ff11f652cefc63fa295e8d1
nicolalandro <nicolaxx94@Live.it>
2022-03-23 08:13:41 +0100

    remove trim, remove unused input, use tempfile also per input

--- streamlit_demo/app.py
+++ streamlit_demo/app.py
@@ -1,13 +1,11 @@
 import tempfile
 
-import librosa
 import numpy as np
 import pysinsy
 import soundfile as sf
 import streamlit as st
 from nnmnkwii.io import hts
 from nnsvs.pretrained import create_svs_engine
-from nnsvs.svs import SPSVS
 
 st.title("NNSVS Demo")
 st.markdown("Upload your .xml music file with text as input to make it sing.")
@@ -23,18 +21,16 @@ uploaded_file = st.file_uploader("Choose a .xml music file", type="xml")
 if st.button("synthesis") and uploaded_file:
     with st.spinner(f"Synthesizing to wav"):
         # synthesize
-        contexts = pysinsy.extract_fullcontext(uploaded_file.name)
+        with tempfile.NamedTemporaryFile(suffix=".xml") as f:
+            f.write(uploaded_file.getbuffer())
+            contexts = pysinsy.extract_fullcontext(f.name)
         labels = hts.HTSLabelFile.create_from_contexts(contexts)
 
         engine = create_svs_engine(models[voice_option])
         wav, sr = engine.svs(labels)
 
-        wav = librosa.effects.trim(wav.astype(np.float64), top_db=40)[0].astype(
-            np.int16
-        )
-
         # show audio player
         with tempfile.NamedTemporaryFile(suffix=".wav") as f:
-            sf.write(f.name, wav, sr)
+            sf.write(f.name, wav.astype(np.int16), sr)
             with open(f.name, "rb") as wav_file:
                 st.audio(wav_file.read(), format="audio/wav")
