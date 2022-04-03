823028d96a4a21e38db277e447e608b2c5cee3b0
Ryuichi Yamamoto <zryuichi@gmail.com>
2022-04-03 11:14:19 +0900

    pass scalers to all training

--- recipes/_common/spsvs/train_acoustic.sh
+++ recipes/_common/spsvs/train_acoustic.sh
@@ -29,6 +29,8 @@ xrun nnsvs-train $ext $hydra_opt \
     data.train_no_dev.out_dir=$dump_norm_dir/$train_set/out_acoustic/ \
     data.dev.in_dir=$dump_norm_dir/$dev_set/in_acoustic/ \
     data.dev.out_dir=$dump_norm_dir/$dev_set/out_acoustic/ \
+    data.in_scaler_path=$dump_norm_dir/in_acoustic_scaler.joblib \
+    data.out_scaler_path=$dump_norm_dir/out_acoustic_scaler.joblib \
     train.out_dir=$expdir/${acoustic_model} \
     train.log_dir=tensorboard/${expname}_${acoustic_model} \
     train.resume.checkpoint=$resume_checkpoint $post_args
--- recipes/_common/spsvs/train_timelag.sh
+++ recipes/_common/spsvs/train_timelag.sh
@@ -29,6 +29,8 @@ xrun nnsvs-train $ext $hydra_opt \
     data.train_no_dev.out_dir=$dump_norm_dir/$train_set/out_timelag/ \
     data.dev.in_dir=$dump_norm_dir/$dev_set/in_timelag/ \
     data.dev.out_dir=$dump_norm_dir/$dev_set/out_timelag/ \
+    data.in_scaler_path=$dump_norm_dir/in_timelag_scaler.joblib \
+    data.out_scaler_path=$dump_norm_dir/out_timelag_scaler.joblib \
     train.out_dir=$expdir/${timelag_model} \
     train.log_dir=tensorboard/${expname}_${timelag_model} \
     train.resume.checkpoint=$resume_checkpoint $post_args
