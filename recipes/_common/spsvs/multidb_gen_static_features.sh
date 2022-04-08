2c6933b0c1e800af49943d915f0fcaff54309b96
Ryuichi Yamamoto <zryuichi@gmail.com>
2022-04-08 23:32:18 +0900

    Add preprocesssing shells for multiple DBs

---
+++ recipes/_common/spsvs/multidb_gen_static_features.sh
@@ -0,0 +1,18 @@
+# NOTE: the script is supposed to be used called from nnsvs recipes.
+# Please don't try to run the shell script directory.
+
+for dbname in ${dbnames};
+do
+    for s in ${datasets[@]};
+    do
+        if [ -d conf/prepare_features ]; then
+            ext="--config-dir conf/gen_static_features"
+        else
+            ext=""
+        fi
+        xrun nnsvs-gen-static-features $ext acoustic=$acoustic_features \
+            in_dir=$dump_norm_dir/$s/out_acoustic/ \
+            out_dir=$dump_norm_dir/$s/out_acoustic_static \
+            utt_list=data/$dbname/list/$s.list
+    done
+done
