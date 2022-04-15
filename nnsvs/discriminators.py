828296af4d0537f38d724eebfa3c41fed754ecd8
Ryuichi Yamamoto <zryuichi@gmail.com>
2022-04-15 20:08:13 +0900

    move init_weights to util

--- nnsvs/discriminators.py
+++ nnsvs/discriminators.py
@@ -1,37 +1,6 @@
 import torch
+from nnsvs.util import init_weights
 from torch import nn
-
-
-# Adapted from https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix
-def init_weights(net, init_type="normal", init_gain=0.02):
-    if init_type == "none":
-        return
-
-    def init_func(m):
-        classname = m.__class__.__name__
-        if hasattr(m, "weight") and (
-            classname.find("Conv") != -1 or classname.find("Linear") != -1
-        ):
-            if init_type == "normal":
-                nn.init.normal_(m.weight.data, 0.0, init_gain)
-            elif init_type == "xavier_normal":
-                nn.init.xavier_normal_(m.weight.data, gain=init_gain)
-            elif init_type == "kaiming_normal":
-                nn.init.kaiming_normal_(m.weight.data, a=0, mode="fan_in")
-            elif init_type == "orthogonal":
-                nn.init.orthogonal_(m.weight.data, gain=init_gain)
-            else:
-                raise NotImplementedError(
-                    "initialization method [%s] is not implemented" % init_type
-                )
-            if hasattr(m, "bias") and m.bias is not None:
-                nn.init.constant_(m.bias.data, 0.0)
-        elif classname.find("BatchNorm2d") != -1:
-            # BatchNorm Layer's weight is not a matrix; only normal distribution applies.
-            nn.init.normal_(m.weight.data, 1.0, init_gain)
-            nn.init.constant_(m.bias.data, 0.0)
-
-    net.apply(init_func)
 
 
 class Conv2dGLU(nn.Module):
