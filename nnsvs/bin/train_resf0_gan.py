14371a7b0d48dac349b46d8357d1aaabe21a3f74
Ryuichi Yamamoto <zryuichi@gmail.com>
2022-04-29 17:14:24 +0900

    Implement segment-based GAN training
    similar to PatchGAN's idea

--- nnsvs/bin/train_resf0_gan.py
+++ nnsvs/bin/train_resf0_gan.py
@@ -48,6 +48,7 @@ def train_step(
     adv_use_static_feats_only=True,
     mask_nth_mgc_for_adv_loss=0,
     gan_type="lsgan",
+    adv_segment_length=-1,
 ):
     optG.zero_grad()
     optD.zero_grad()
@@ -108,6 +109,20 @@ def train_step(
             model_config.stream_sizes,
             adv_streams,
         )
+
+    # Rather than classifying whole features as real or fake, use smaller segments
+    # that should ease the training of GANs I guess
+    # The idaa is similar to the trick used for training neural vocoders. i.e.,
+    # use small segments like 8000 or 24000 samples for training vocoders.
+    if adv_segment_length > 0:
+        if lengths.min() < adv_segment_length:
+            real_netD_in_feats = real_netD_in_feats[:, : lengths.min(), :]
+            fake_netD_in_feats = fake_netD_in_feats[:, : lengths.min(), :]
+        else:
+            si = np.random.randint(0, lengths.min() - adv_segment_length)
+            real_netD_in_feats = real_netD_in_feats[:, si : si + adv_segment_length, :]
+            fake_netD_in_feats = fake_netD_in_feats[:, si : si + adv_segment_length, :]
+            in_feats = in_feats[:, si : si + adv_segment_length, :]
 
     # Ref: http://sython.org/papers/ASJ/saito2017asja.pdf
     # 0-th mgc with adversarial trainging affects speech quality
