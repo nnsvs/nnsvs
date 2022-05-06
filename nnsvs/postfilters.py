3c02ef2cd06e725ba432e19b0f55b91567c164b2
Ryuichi Yamamoto <zryuichi@gmail.com>
2022-05-06 22:16:30 +0900

    Refactor postfilrers

--- nnsvs/postfilters.py
+++ nnsvs/postfilters.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 from nnsvs.base import BaseModel
 from nnsvs.multistream import split_streams
@@ -7,45 +8,38 @@ from torch.nn import functional as F
 
 
 class Conv2dPostFilter(BaseModel):
-    def __init__(self, channels=128, init_type="kaiming_normal"):
+    """A post-filter based on Conv2d
+
+    A model proposed in the paper :cite:t:`kaneko2017generative`.
+
+    Args:
+        channels (int): number of channels
+        kernel_size (tuple): kernel sizes for Conv2d
+        init_type (str): type of initialization
+    """
+
+    def __init__(self, channels=128, kernel_size=(5, 5), init_type="kaiming_normal"):
         super().__init__()
         C = channels
-        self.conv1 = nn.Conv2d(1, C, kernel_size=(5, 5), padding=(2, 2))
-        self.conv2 = nn.Conv2d(C, C * 2, kernel_size=(5, 5), padding=(2, 2))
-        self.conv3 = nn.Conv2d(C * 2, C, kernel_size=(5, 5), padding=(2, 2))
-        self.conv4 = nn.Conv2d(C, 1, kernel_size=(5, 5), padding=(2, 2))
+        assert len(kernel_size) == 2
+        ks = np.asarray(list(kernel_size))
+        padding = (ks - 1) // 2
+        self.conv1 = nn.Conv2d(2, C, kernel_size=ks, padding=padding)
+        self.conv2 = nn.Conv2d(C + 1, C * 2, kernel_size=ks, padding=padding)
+        self.conv3 = nn.Conv2d(C * 2 + 1, C, kernel_size=ks, padding=padding)
+        self.conv4 = nn.Conv2d(C + 1, 1, kernel_size=ks, padding=padding)
         init_weights(self, init_type)
 
     def forward(self, x, lengths=None):
-        # (B, T, C) -> (B, 1, T, C):
-        x = x.unsqueeze(1)
-        z = torch.randn_like(x)
-        x_syn = x
+        """Forward step
 
-        y = F.relu(self.conv1(x_syn + z))
-        y = F.relu(self.conv2(x_syn + y))
-        y = F.relu(self.conv3(x_syn + y))
-        residual = self.conv4(x_syn + y)
+        Args:
+            x (torch.Tensor): input tensor of shape (B, T, C)
+            lengths (torch.Tensor): lengths of shape (B,)
 
-        out = x_syn + residual
-
-        # (B, 1, T, C) -> (B, T, C)
-        out = out.squeeze(1)
-
-        return out
-
-
-class Conv2dPostFilter2(BaseModel):
-    def __init__(self, channels=128, init_type="kaiming_normal"):
-        super().__init__()
-        C = channels
-        self.conv1 = nn.Conv2d(2, C, kernel_size=(5, 5), padding=(2, 2))
-        self.conv2 = nn.Conv2d(C + 1, C * 2, kernel_size=(5, 5), padding=(2, 2))
-        self.conv3 = nn.Conv2d(C * 2 + 1, C, kernel_size=(5, 5), padding=(2, 2))
-        self.conv4 = nn.Conv2d(C + 1, 1, kernel_size=(5, 5), padding=(2, 2))
-        init_weights(self, init_type)
-
-    def forward(self, x, lengths=None):
+        Returns:
+            torch.Tensor: output tensor of shape (B, T, C)
+        """
         # (B, T, C) -> (B, 1, T, C):
         x = x.unsqueeze(1)
         z = torch.randn_like(x)
@@ -64,43 +58,78 @@ class Conv2dPostFilter2(BaseModel):
         return out
 
 
-class MGCPostFilter(BaseModel):
+class MultistreamPostFilter(BaseModel):
+    """A multi-stream post-filter that applies post-filtering for each feature stream
+
+    Currently, post-filtering for MGC, BAP and log-F0 are supported.
+    Note that it doesn't make much sense to apply post-filtering for other features.
+
+    Args:
+        mgc_postfilter (nn.Module): post-filter for MGC
+        bap_postfilter (nn.Module): post-filter for BAP
+        lf0_postfilter (nn.Module): post-filter for log-F0
+        stream_sizes (list): sizes of each feature stream
+        mgc_offset (int): offset for MGC. Defaults to 2.
+    """
+
     def __init__(
         self,
-        timbre_model: nn.Module,
+        mgc_postfilter: nn.Module,
+        bap_postfilter: nn.Module,
+        lf0_postfilter: nn.Module,
         stream_sizes: list,
+        mgc_offset: int = 2,
     ):
         super().__init__()
-        self.timbre_model = timbre_model
+        self.mgc_postfilter = mgc_postfilter
+        self.bap_postfilter = bap_postfilter
+        self.lf0_postfilter = lf0_postfilter
         self.stream_sizes = stream_sizes
+        self.mgc_offset = mgc_offset
 
     def forward(self, x, lengths=None):
-        mgc, lf0, vuv, bap, vib, vib_flags = split_streams(x, self.stream_sizes)
+        """Forward step
 
-        mgc_pf = self.timbre_model(mgc, lengths)
+        Each feature stream is processed independently.
 
-        out = torch.cat([mgc_pf, lf0, vuv, bap, vib, vib_flags], dim=-1)
+        Args:
+            x (torch.Tensor): input tensor of shape (B, T, C)
+            lengths (torch.Tensor): lengths of shape (B,)
 
-        return out
+        Returns:
+            torch.Tensor: output tensor of shape (B, T, C)
+        """
+        streams = split_streams(x, self.stream_sizes)
+        if len(streams) == 4:
+            mgc, lf0, vuv, bap = streams
+        elif len(streams) == 5:
+            mgc, lf0, vuv, bap, vuv = streams
+        elif len(streams) == 6:
+            mgc, lf0, vuv, bap, vib, vib_flags = streams
+        else:
+            raise ValueError("Invalid number of streams")
 
+        if self.mgc_postfilter is not None:
+            if self.mgc_offset > 0:
+                # keep unchanged for the 0-to-${mgc_offset}-th dim of mgc
+                mgc0 = mgc[:, :, : self.mgc_offset]
+                mgc_pf = self.mgc_postfilter(mgc[:, :, self.mgc_offset :], lengths)
+                mgc_pf = torch.cat([mgc0, mgc_pf], dim=-1)
+            else:
+                mgc_pf = self.mgc_postfilter(mgc, lengths)
+            mgc = mgc_pf
 
-class MGCPostFilter2(BaseModel):
-    def __init__(
-        self,
-        timbre_model: nn.Module,
-        stream_sizes: list,
-    ):
-        super().__init__()
-        self.timbre_model = timbre_model
-        self.stream_sizes = stream_sizes
+        if self.bap_postfilter is not None:
+            bap = self.bap_postfilter(bap, lengths)
 
-    def forward(self, x, lengths=None):
-        mgc, lf0, vuv, bap, vib, vib_flags = split_streams(x, self.stream_sizes)
+        if self.lf0_postfilter is not None:
+            lf0 = self.lf0_postfilter(lf0, lengths)
 
-        mgc0 = mgc[:, :, 0:1]
-        mgc1 = mgc[:, :, 1:]
-        mgc1_pf = self.timbre_model(mgc1, lengths)
-
-        out = torch.cat([mgc0, mgc1_pf, lf0, vuv, bap, vib, vib_flags], dim=-1)
+        if len(streams) == 4:
+            out = torch.cat([mgc, lf0, vuv, bap], dim=-1)
+        elif len(streams) == 5:
+            out = torch.cat([mgc, lf0, vuv, bap, vib], dim=-1)
+        elif len(streams) == 6:
+            out = torch.cat([mgc, lf0, vuv, bap, vib, vib_flags], dim=-1)
 
         return out
