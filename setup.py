43f0efee2ce8aea32e940ff18b895ce282c9ad9b
Ryuichi Yamamoto <zryuichi@gmail.com>
2022-05-21 00:51:25 +0900

    move train_resf0_gan.py to experimantal

--- setup.py
+++ setup.py
@@ -74,7 +74,6 @@ setup(
             "nnsvs-preprocess-normalize = nnsvs.bin.preprocess_normalize:entry",
             "nnsvs-train = nnsvs.bin.train:entry",
             "nnsvs-train-resf0 = nnsvs.bin.train_resf0:entry",
-            "nnsvs-train-resf0-gan = nnsvs.bin.train_resf0_gan:entry",
             "nnsvs-train-postfilter = nnsvs.bin.train_postfilter:entry",
             "nnsvs-generate = nnsvs.bin.generate:entry",
             "nnsvs-gen-static-features = nnsvs.bin.gen_static_features:entry",
