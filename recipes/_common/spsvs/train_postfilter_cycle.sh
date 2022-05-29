a1f6726ebc7c5f7042193c0b9e6b2ecab2f725d6
Ryuichi Yamamoto <zryuichi@gmail.com>
2022-05-30 02:06:56 +0900

    Add training script for cyclegan-based post-filter

---
+++ recipes/_common/spsvs/train_postfilter_cycle.sh
@@ -0,0 +1,43 @@
+# NOTE: the script is supposed to be used called from nnsvs recipes.
+# Please don't try to run the shell script directory.
+
+if [ -d conf/train_postfilter ]; then
+    ext="--config-dir conf/train_postfilter_cycle"
+else
+    ext=""
+fi
+
+if [ ! -z "${pretrained_expdir}" ]; then
+    resume_checkpoint_g=$pretrained_expdir/${postfilter_model}/latest.pth
+    if [ -e $pretrained_expdir/${postfilter_model}/latest_D.pth ]; then
+        resume_checkpoint_d=$pretrained_expdir/${postfilter_model}/latest_D.pth
+    else
+        resume_checkpoint_d=
+    fi
+else
+    resume_checkpoint_g=
+    resume_checkpoint_d=
+fi
+
+# Hyperparameter search with Hydra + optuna
+# mlflow is used to log the results of the hyperparameter search
+if [[ ${postfilter_hydra_optuna_sweeper_args+x} && ! -z $postfilter_hydra_optuna_sweeper_args ]]; then
+    hydra_opt="-m ${postfilter_hydra_optuna_sweeper_args}"
+    post_args="mlflow.enabled=true mlflow.experiment=${expname}_${postfilter_model} hydra.sweeper.n_trials=${postfilter_hydra_optuna_sweeper_n_trials}"
+else
+    hydra_opt=""
+    post_args=""
+fi
+
+xrun nnsvs-train-postfilter-cycle $ext $hydra_opt \
+    model=$postfilter_model train=$postfilter_train data=$postfilter_data \
+    data.train_no_dev.in_dir=$expdir/$acoustic_model/norm/$train_set/in_postfilter \
+    data.train_no_dev.out_dir=$dump_norm_dir/$train_set/out_postfilter \
+    data.dev.in_dir=$expdir/$acoustic_model/norm/$dev_set/in_postfilter \
+    data.dev.out_dir=$dump_norm_dir/$dev_set/out_postfilter \
+    data.in_scaler_path=$expdir/$acoustic_model/norm/in_postfilter_scaler.joblib \
+    data.out_scaler_path=$dump_norm_dir/out_postfilter_scaler.joblib \
+    train.out_dir=$expdir/${postfilter_model} \
+    train.log_dir=tensorboard/${expname}_${postfilter_model} \
+    train.resume.netG.checkpoint=$resume_checkpoint_g \
+    train.resume.netD.checkpoint=$resume_checkpoint_d $post_args
