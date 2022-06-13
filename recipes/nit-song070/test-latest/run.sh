8a2e7de80c0083b952398f34cb5b43f16b8e5d5a
Ryuichi Yamamoto <zryuichi@gmail.com>
2022-06-14 00:27:53 +0900

    Include post-filter/NSF steps to the test

--- recipes/nit-song070/test-latest/run.sh
+++ recipes/nit-song070/test-latest/run.sh
@@ -105,9 +105,60 @@ if [ ${stage} -le 6 ] && [ ${stop_stage} -ge 6 ]; then
     . $NNSVS_COMMON_ROOT/synthesis_resf0.sh
 fi
 
+if [ ${stage} -le 7 ] && [ ${stop_stage} -ge 7 ]; then
+    echo "stage 7: Prepare input/output features for post-filter"
+    . $NNSVS_COMMON_ROOT/prepare_postfilter.sh
+fi
+
+if [ ${stage} -le 8 ] && [ ${stop_stage} -ge 8 ]; then
+    echo "stage 8: Training post-filter"
+    . $NNSVS_COMMON_ROOT/train_postfilter.sh
+fi
+
+if [ ${stage} -le 9 ] && [ ${stop_stage} -ge 9 ]; then
+    echo "stage 9: Prepare vocoder input/output features"
+    . $NNSVS_COMMON_ROOT/prepare_voc_features.sh
+fi
+
+if [ ${stage} -le 10 ] && [ ${stop_stage} -ge 10 ]; then
+    echo "stage 10: Compute statistics of vocoder's input features"
+    xrun python $NNSVS_COMMON_ROOT/scaler_joblib2npy_voc.py \
+        $dump_norm_dir/out_acoustic_scaler.joblib $dump_norm_dir/
+fi
+
+if [ ${stage} -le 11 ] && [ ${stop_stage} -ge 11 ]; then
+    echo "stage 11: Training vocoder using parallel_wavegan"
+    if [ ! -z ${pretrained_vocoder_checkpoint} ]; then
+        extra_args="--resume $pretrained_vocoder_checkpoint"
+    else
+        extra_args=""
+    fi
+    # NOTE: copy normalization stats to expdir for convenience
+    mkdir -p $expdir/$vocoder_model
+    cp -v $dump_norm_dir/in_vocoder*.npy $expdir/$vocoder_model
+    xrun parallel-wavegan-train --config conf/parallel_wavegan/${vocoder_model}.yaml \
+        --train-dumpdir $dump_norm_dir/$train_set/in_vocoder \
+        --dev-dumpdir $dump_norm_dir/$dev_set/in_vocoder/ \
+        --outdir $expdir/$vocoder_model $extra_args
+fi
+
+if [ ${stage} -le 12 ] && [ ${stop_stage} -ge 12 ]; then
+    echo "stage 12: Synthesis waveforms by parallel_wavegan"
+    if [ -z "${vocoder_eval_checkpoint}" ]; then
+        vocoder_eval_checkpoint="$(ls -dt "${expdir}/${vocoder_model}"/*.pkl | head -1 || true)"
+    fi
+    outdir="${expdir}/$vocoder_model/wav/$(basename "${vocoder_eval_checkpoint}" .pkl)"
+    for s in ${testsets[@]}; do
+        xrun parallel-wavegan-decode --dumpdir $dump_norm_dir/$s/in_vocoder \
+            --checkpoint $vocoder_eval_checkpoint \
+            --outdir $outdir
+    done
+fi
+
+
 if [ ${stage} -le 99 ] && [ ${stop_stage} -ge 99 ]; then
     echo "Pack models for SVS"
-    if [[ -z "${vocoder_eval_checkpoint}" && -d ${expdir}/${vocoder_model}/config.yml ]]; then
+    if [[ -z "${vocoder_eval_checkpoint}" && -f ${expdir}/${vocoder_model}/config.yml ]]; then
         vocoder_eval_checkpoint="$(ls -dt "$expdir/$vocoder_model"/*.pkl | head -1 || true)"
     fi
     # Determine the directory name of a packed model
