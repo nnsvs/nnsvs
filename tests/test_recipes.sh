20a227a80e6ab41f69d12095d26244d647565cb7
Ryuichi Yamamoto <zryuichi@gmail.com>
2022-06-19 23:33:51 +0900

    Fix recipe test

--- tests/test_recipes.sh
+++ tests/test_recipes.sh
@@ -10,44 +10,43 @@ cd $NNSVS_ROOT/recipes/nit-song070/test-latest
 
 # Normal setup
 ./run.sh --stage -1 --stop-stage 6 \
-    --timelag-model timelag_mdn \
-    --duration-model duration_mdn \
-    --acoustic_model acoustic_resf0conv
+    --timelag-model timelag_test \
+    --duration-model duration_test \
+    --acoustic_model acoustic_test
 
 # Generate training data for post-filtering
 ./run.sh --stage 7 --stop-stage 7 \
-    --timelag-model timelag_mdn \
-    --duration-model duration_mdn \
-    --acoustic_model acoustic_resf0conv \
-    --postfilter_model postfilter_mgc --postfilter_train mgc
+    --timelag-model timelag_test \
+    --duration-model duration_test \
+    --acoustic_model acoustic_test
 
 # Train post-filter for mgc/bap separately
 # NOTE: we must use specific model/train configs for mgc/bap respectively.
-for typ in mgc bap;
+for typ in mgc_test bap_test;
 do
     ./run.sh --stage 8 --stop-stage 8 \
-        --timelag-model timelag_mdn \
-        --duration-model duration_mdn \
-        --acoustic_model acoustic_resf0conv \
+        --timelag-model timelag_test \
+        --duration-model duration_test \
+        --acoustic_model acoustic_test \
         --postfilter_model postfilter_${typ} --postfilter_train ${typ}
 done
 # Merge post-filters
 python $NNSVS_ROOT/utils/merge_postfilters.py \
-    exp/yoko/postfilter_mgc/latest.pth \
-    exp/yoko/postfilter_bap/latest.pth \
+    exp/yoko/postfilter_mgc_test/latest.pth \
+    exp/yoko/postfilter_bap_test/latest.pth \
     exp/yoko/postfilter_merged
 
 # Train neural vocoder
 ./run.sh --stage 9 --stop-stage 12 \
-    --timelag-model timelag_mdn \
-    --duration-model duration_mdn \
-    --acoustic_model acoustic_resf0conv \
-    --vocoder_model hn-sinc-nsf_sr48k_pwgD
+    --timelag-model timelag_test \
+    --duration-model duration_test \
+    --acoustic_model acoustic_test \
+    --vocoder_model hn-sinc-nsf_sr48k_pwgD_test
 
 # Run the packaging step
 ./run.sh --stage 99 --stop-stage 99 \
-    --timelag-model timelag_mdn \
-    --duration-model duration_mdn \
-    --acoustic_model acoustic_resf0conv \
+    --timelag-model timelag_test \
+    --duration-model duration_test \
+    --acoustic_model acoustic_test \
     --postfilter_model postfilter_merged \
-    --vocoder_model hn-sinc-nsf_sr48k_pwgD
+    --vocoder_model hn-sinc-nsf_sr48k_pwgD_test
