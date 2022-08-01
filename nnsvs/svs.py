762bb6a8598d128b3b0f5f4348e05fe0e5f611e6
Tarou Shirani <taroushirani@gmail.com>
2022-08-01 23:39:59 +0900

    Fix device assignment

--- nnsvs/svs.py
+++ nnsvs/svs.py
@@ -335,14 +335,13 @@ WORLD is only supported for waveform generation"""
         # Learned post-filter using nnsvs
         if post_filter_type == "nnsvs" and self.postfilter_model is not None:
             in_feats = (
-                torch.from_numpy(acoustic_features).float().to(self.device).unsqueeze(0)
+                torch.from_numpy(acoustic_features).float().unsqueeze(0)
             )
-            in_feats = self.postfilter_out_scaler.transform(in_feats).float()
+            in_feats = self.postfilter_out_scaler.transform(in_feats).float().to(self.device)
             out_feats = self.postfilter_model.inference(in_feats, [in_feats.shape[1]])
             acoustic_features = (
-                self.postfilter_out_scaler.inverse_transform(out_feats)
+                self.postfilter_out_scaler.inverse_transform(out_feats.cpu())
                 .squeeze(0)
-                .cpu()
                 .numpy()
             )
 
