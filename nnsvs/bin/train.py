afa40fc5728422eae40cc56ca64e6779b35ee5b7
Ryuichi Yamamoto <zryuichi@gmail.com>
2022-10-30 22:53:22 +0900

    update train.py

--- nnsvs/bin/train.py
+++ nnsvs/bin/train.py
@@ -5,11 +5,13 @@ import hydra
 import mlflow
 import numpy as np
 import torch
+import torch.distributed as dist
 from hydra.utils import to_absolute_path
 from nnmnkwii import metrics
 from nnsvs.base import PredictionType
 from nnsvs.mdn import mdn_get_most_probable_sigma_and_mu, mdn_loss
 from nnsvs.multistream import split_streams
+from nnsvs.util import PyTorchStandardScaler, make_non_pad_mask
 from nnsvs.train_util import (
     collate_fn_default,
     collate_fn_random_segments,
@@ -19,11 +21,9 @@ from nnsvs.train_util import (
     save_configs,
     setup,
 )
-from nnsvs.util import PyTorchStandardScaler, make_non_pad_mask
 from omegaconf import DictConfig
 from torch import nn
 from torch.cuda.amp import autocast
-from tqdm import tqdm
 
 
 @torch.no_grad()
@@ -144,6 +144,7 @@ def train_loop(
     lr_scheduler,
     grad_scaler,
     data_loaders,
+    samplers,
     writer,
     out_scaler,
     use_mlflow,
@@ -160,19 +161,33 @@ def train_loop(
     else:
         feats_criterion = config.train.feats_criterion
 
+    if dist.is_initialized() and dist.get_rank() != 0:
+
+        def tqdm(x, **kwargs):
+            return x
+
+    else:
+        from tqdm import tqdm
+
+    train_iter = 1
     for epoch in tqdm(range(1, config.train.nepochs + 1)):
         for phase in data_loaders.keys():
             train = phase.startswith("train")
+            # https://pytorch.org/docs/stable/data.html#torch.utils.data.distributed.DistributedSampler
+            if dist.is_initialized() and train and samplers[phase] is not None:
+                samplers[phase].set_epoch(epoch)
             running_loss = 0
             running_metrics = {}
-            for in_feats, out_feats, lengths in data_loaders[phase]:
+            for in_feats, out_feats, lengths in tqdm(
+                data_loaders[phase], desc=f"{phase} iter", leave=False
+            ):
                 # NOTE: This is needed for pytorch's PackedSequence
                 lengths, indices = torch.sort(lengths, dim=0, descending=True)
                 in_feats, out_feats = (
                     in_feats[indices].to(device),
                     out_feats[indices].to(device),
                 )
-                loss, distortions = train_step(
+                loss, log_metrics = train_step(
                     model=model,
                     optimizer=optimizer,
                     grad_scaler=grad_scaler,
@@ -186,8 +201,15 @@ def train_loop(
                     stream_weights=config.model.stream_weights,
                     stream_sizes=config.model.stream_sizes,
                 )
+
+                if train:
+                    if writer is not None:
+                        for key, val in log_metrics.items():
+                            writer.add_scalar(f"{key}_Step/{phase}", val, train_iter)
+                    train_iter += 1
+
                 running_loss += loss.item()
-                for k, v in distortions.items():
+                for k, v in log_metrics.items():
                     try:
                         running_metrics[k] += float(v)
                     except KeyError:
@@ -201,11 +223,15 @@ def train_loop(
 
             ave_loss = running_loss / len(data_loaders[phase])
             logger.info("[%s] [Epoch %s]: loss %s", phase, epoch, ave_loss)
+            if writer is not None:
+                writer.add_scalar(f"Loss_Epoch/{phase}", ave_loss, epoch)
+            if use_mlflow:
+                mlflow.log_metric(f"{phase}_loss", ave_loss, step=epoch)
 
             for k, v in running_metrics.items():
                 ave_v = v / len(data_loaders[phase])
                 if writer is not None:
-                    writer.add_scalar(f"{k}/{phase}", ave_v, epoch)
+                    writer.add_scalar(f"{k}_Epoch/{phase}", ave_v, epoch)
                 if use_mlflow:
                     mlflow.log_metric(f"{phase}_{k}", ave_v, step=epoch)
 
@@ -243,6 +269,12 @@ def my_app(config: DictConfig) -> None:
     else:
         collate_fn = collate_fn_default
 
+    if config.train.use_ddp:
+        dist.init_process_group("nccl")
+        rank = dist.get_rank()
+        device_id = rank % torch.cuda.device_count()
+        torch.cuda.set_device(device_id)
+
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     (
         model,
@@ -250,6 +282,7 @@ def my_app(config: DictConfig) -> None:
         lr_scheduler,
         grad_scaler,
         data_loaders,
+        samplers,
         writer,
         logger,
         _,
@@ -268,32 +301,34 @@ def my_app(config: DictConfig) -> None:
             save_configs(config)
             log_params_from_omegaconf_dict(config)
             last_dev_loss = train_loop(
-                config,
-                logger,
-                device,
-                model,
-                optimizer,
-                lr_scheduler,
-                grad_scaler,
-                data_loaders,
-                writer,
-                out_scaler,
-                use_mlflow,
+                config=config,
+                logger=logger,
+                device=device,
+                model=model,
+                optimizer=optimizer,
+                lr_scheduler=lr_scheduler,
+                grad_scaler=grad_scaler,
+                data_loaders=data_loaders,
+                samplers=samplers,
+                writer=writer,
+                out_scaler=out_scaler,
+                use_mlflow=use_mlflow,
             )
     else:
         save_configs(config)
         last_dev_loss = train_loop(
-            config,
-            logger,
-            device,
-            model,
-            optimizer,
-            lr_scheduler,
-            grad_scaler,
-            data_loaders,
-            writer,
-            out_scaler,
-            use_mlflow,
+            config=config,
+            logger=logger,
+            device=device,
+            model=model,
+            optimizer=optimizer,
+            lr_scheduler=lr_scheduler,
+            grad_scaler=grad_scaler,
+            data_loaders=data_loaders,
+            samplers=samplers,
+            writer=writer,
+            out_scaler=out_scaler,
+            use_mlflow=use_mlflow,
         )
 
     return last_dev_loss
