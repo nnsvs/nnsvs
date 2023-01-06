c0ba3676ac2276993396b23c7d988e963eab84c6
TrellixVulnTeam <charles.mcfarland@trellix.com>
2023-01-06 01:45:52 +0000

    Adding tarfile member sanitization to extractall()

--- nnsvs/pretrained/__init__.py
+++ nnsvs/pretrained/__init__.py
@@ -126,7 +126,26 @@ Available models:
             urlretrieve(url, filename, reporthook=t.update_to)
             t.total = t.n
         with tarfile.open(filename, mode="r|gz") as f:
-            f.extractall(path=model_dir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner) 
+                
+            
+            safe_extract(f, path=model_dir)
         os.remove(filename)
 
     return out_dir
