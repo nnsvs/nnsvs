e0ccdc34b9fec3c54254910f93b5bb5bb8a22f4f
Ryuichi Yamamoto <zryuichi@gmail.com>
2023-01-08 19:34:45 +0900

    Revert "logger: add_stream_handler option"
    This reverts commit ecd5cc097c03137578caaace74653987da9f135f.

--- nnsvs/logger.py
+++ nnsvs/logger.py
@@ -8,7 +8,7 @@ from os.path import dirname
 format = "%(asctime)s (%(module)s:%(lineno)d) %(levelname)s: %(message)s"
 
 
-def getLogger(verbose=0, filename=None, name="nnsvs", add_stream_handler=False):
+def getLogger(verbose=0, filename=None, name="nnsvs"):
     logger = logging.getLogger(name)
     if verbose >= 100:
         logger.setLevel(logging.DEBUG)
@@ -19,8 +19,7 @@ def getLogger(verbose=0, filename=None, name="nnsvs", add_stream_handler=False):
 
     stream_handler = logging.StreamHandler()
     stream_handler.setFormatter(logging.Formatter(format))
-    if add_stream_handler:
-        logger.addHandler(stream_handler)
+    # logger.addHandler(stream_handler)
 
     if filename is not None:
         os.makedirs(dirname(filename), exist_ok=True)
