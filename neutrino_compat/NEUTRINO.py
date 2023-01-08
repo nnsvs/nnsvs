6d210fad42f1887c7eae71459194abb89eb595ce
Ryuichi Yamamoto <zryuichi@gmail.com>
2023-01-08 21:23:44 +0900

    neutrino_compat: add client impl for NEUTRINO.py and NSF.py

--- neutrino_compat/NEUTRINO.py
+++ neutrino_compat/NEUTRINO.py
@@ -19,12 +19,15 @@ usage:
 """
 import argparse
 import sys
+import time
 from pathlib import Path
 
+import numpy as np
+import requests
 import torch
 from nnmnkwii.io import hts
 from nnsvs.io.hts import full_to_mono
-from nnsvs.svs import NEUTRINO
+from nnsvs.logger import getLogger
 
 
 def get_parser():
@@ -42,14 +45,21 @@ def get_parser():
     ),
     parser.add_argument(
         "-p", "--phrase_num", type=int, default=-1, help="Phrase number"
-    ),
+    )
+    parser.add_argument("--use_api", action="store_true", help="Use web API")
+    parser.add_argument(
+        "--url", type=str, default="http://127.0.0.1:8001", help="URL of the server"
+    )
     return parser
 
 
-def main():
-    args = get_parser().parse_args(sys.argv[1:])
+def run_local(args, _):
+    from nnsvs.svs import NEUTRINO
+
     model_dir = Path(args.model_dir)
-    engine = NEUTRINO(model_dir, device="cuda" if torch.cuda.is_available() else "cpu")
+    engine = NEUTRINO(
+        model_dir, device="cuda" if torch.cuda.is_available() else "cpu", verbose=100
+    )
 
     full_lab = Path(args.full_lab)
     assert full_lab.exists()
@@ -76,11 +86,104 @@ def main():
     f0, mgc, bap = engine.predict_acoustic(
         full_labels, timing_labels, phrase_num=args.phrase_num
     )
+    return f0, mgc, bap
+
+
+def run_api(args, logger):
+    full_lab = Path(args.full_lab)
+    url = args.url[:-1] if args.url[-1] == "/" else args.url
+    assert full_lab.exists()
+
+    name = full_lab.stem
+    # pretend as if the model_dir is model_id
+    model_id = args.model_dir
+
+    # Upload full-context labels
+    logger.info(f"Uploading full_lab: {full_lab}")
+    res = requests.post(
+        url + "/score/full/upload",
+        files={
+            "full_lab": open(full_lab, "rb"),
+        },
+    )
+    if res.status_code != 200:
+        raise RuntimeError(f"Failed to upload full_lab: {res.status_code}")
+
+    # Predict timing
+    logger.info("Predicting timing")
+    res = requests.get(
+        url + "/run/timing",
+        params={
+            "name": name,
+            "model_id": model_id,
+        },
+    )
+    if res.status_code != 200:
+        raise RuntimeError(f"Failed to predict timing: {res.status_code}")
+    timing_str = res.json()["timing"]
+    logger.info(timing_str)
+    with open(args.timing_lab, "w") as f:
+        f.write(timing_str)
+
+    # Phraselist
+    if args.phraselist is not None:
+        logger.info("Predicting phraselist")
+        res = requests.get(
+            url + "/run/phrases",
+            params={
+                "name": name,
+                "model_id": model_id,
+            },
+        )
+        if res.status_code != 200:
+            raise RuntimeError(f"Failed to predict phraselist: {res.status_code}")
+        phraselist_str = res.json()["phraselist"]
+        logger.info(phraselist_str)
+        with open(args.phraselist, "w") as f:
+            f.write(phraselist_str)
+
+    # Predict acoustic features
+    logger.info("Predicting acoustic features")
+    res = requests.get(
+        url + "/run/acoustic",
+        params={
+            "name": name,
+            "model_id": model_id,
+            "phrase_num": args.phrase_num,
+        },
+    )
+    if res.status_code != 200:
+        raise RuntimeError(f"Failed to predict acoustic features: {res.status_code}")
+
+    feats = np.frombuffer(res.content, dtype=np.float64).reshape(-1, 66)
+    f0 = feats[:, :1]
+    mgc = feats[:, 1:61]
+    bap = feats[:, 61:]
+    logger.info(f"f0: {f0.shape}")
+    logger.info(f"mgc: {mgc.shape}")
+    logger.info(f"bap: {bap.shape}")
+
+    return f0, mgc, bap
+
+
+def main():
+    args = get_parser().parse_args(sys.argv[1:])
+
+    start_time = time.time()
+    logger = getLogger(verbose=100, name="neutrino")
+    if args.use_api:
+        logger.info(f"Using webapi: {args.url}")
+        f0, mgc, bap = run_api(args, logger)
+    else:
+        logger.info("Using local machine")
+        f0, mgc, bap = run_local(args, logger)
 
     # Save to file
     f0.tofile(args.output_f0)
     mgc.tofile(args.output_mgc)
     bap.tofile(args.output_bap)
+
+    logger.info(f"Elapsed time: {time.time() - start_time:.2f} sec")
 
 
 if __name__ == "__main__":
--- neutrino_compat/NSF.py
+++ neutrino_compat/NSF.py
@@ -18,12 +18,14 @@ usage:
 """
 import argparse
 import sys
+import time
 from pathlib import Path
 
 import numpy as np
 import pyworld
+import requests
 import torch
-from nnsvs.svs import NEUTRINO
+from nnsvs.logger import getLogger
 from scipy.io import wavfile
 
 
@@ -36,11 +38,16 @@ def get_parser():
     parser.add_argument("input_bap", type=str, help="Path of input BAP")
     parser.add_argument("model_dir", type=str, help="model_dir")
     parser.add_argument("output_wav", type=str, help="Path of output wav")
+    parser.add_argument("--use_api", action="store_true", help="Use web API")
+    parser.add_argument(
+        "--url", type=str, default="http://127.0.0.1:8001", help="URL of the server"
+    )
     return parser
 
 
-def main():
-    args = get_parser().parse_args(sys.argv[1:])
+def run_local(args, _):
+    from nnsvs.svs import NEUTRINO
+
     model_dir = Path(args.model_dir)
     engine = NEUTRINO(model_dir, device="cuda" if torch.cuda.is_available() else "cpu")
 
@@ -54,7 +61,79 @@ def main():
     # and fallback to WORLD it doesn't exist.
     wav = engine.predict_waveform(f0, mgc, bap, vocoder_type="auto", dtype=np.int16)
 
-    wavfile.write(args.output_wav, engine.sample_rate, wav)
+    return wav, engine.sample_rate
+
+
+def run_api(args, logger):
+    url = args.url[:-1] if args.url[-1] == "/" else args.url
+
+    # NOTE: for webAPI, these local files are not used for inference
+    # files on the remote server are used instead.
+    input_f0 = Path(args.input_f0)
+    input_mgc = Path(args.input_mgc)
+    input_bap = Path(args.input_bap)
+    assert input_f0.exists()
+    assert input_mgc.exists()
+    assert input_bap.exists()
+
+    name = input_f0.stem
+    # TODO: better way to handle phrase-based synthesis
+    # At the moment, we need to tell the server which phrase we are synthesizing
+    if "-" in name:
+        # NOTE: this code does not work for complicated filenames
+        phrase_num = int(name.split("-")[-1])
+        name = name.split("-")[0]
+    else:
+        phrase_num = -1
+
+    # pretend as if the model_dir is model_id
+    model_id = args.model_dir
+
+    # Get sampling rate
+    res = requests.get(
+        url + f"/models/{model_id}",
+    )
+    if res.status_code != 200:
+        raise RuntimeError(f"Failed to fetch model info: {res.status_code}")
+    res = res.json()
+    sample_rate = res["config"]["sample_rate"]
+
+    # Run vocoder
+    logger.info("Predicting waveform")
+    dtype = "int16"
+    res = requests.get(
+        url + "/run/vocoder",
+        params={
+            "name": name,
+            "model_id": model_id,
+            "phrase_num": phrase_num,
+            "vocoder_type": "auto",
+            "dtype": dtype,
+            "loudness_norm": False,
+        },
+    )
+    if res.status_code != 200:
+        raise RuntimeError(f"Failed to generate waveform: {res.status_code}")
+
+    wav = np.frombuffer(res.content, dtype=dtype).reshape(-1)
+
+    return wav, sample_rate
+
+
+def main():
+    args = get_parser().parse_args(sys.argv[1:])
+
+    start_time = time.time()
+    logger = getLogger(verbose=100, name="neutrino")
+    if args.use_api:
+        logger.info(f"Using webapi: {args.url}")
+        wav, sr = run_api(args, logger)
+    else:
+        logger.info("Using local machine")
+        wav, sr = run_local(args, logger)
+
+    wavfile.write(args.output_wav, sr, wav)
+    logger.info(f"Elapsed time: {time.time() - start_time:.2f} sec")
 
 
 if __name__ == "__main__":
