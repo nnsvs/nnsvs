346b8b67293ba01a382445858e702202659d0b6c
Ryuichi Yamamoto <zryuichi@gmail.com>
2023-03-08 22:45:21 +0900

    Add check for fastdtw issue

--- recipes/_common/no2/utils/align_lab.py
+++ recipes/_common/no2/utils/align_lab.py
@@ -3,15 +3,38 @@ import sys
 from glob import glob
 from os.path import basename, join
 
+import numpy as np
 import yaml
 from fastdtw import fastdtw
 from nnmnkwii.io import hts
 from tqdm import tqdm
 from util import fix_mono_lab_after_align, ph2numeric, prep_ph2num
 
+
+def check_fastdtw_correctness():
+    x = [1, 6, 6, 33, 35]
+    y = [1, 6, 6, 33, 35]
+
+    d, path = fastdtw(x, y, radius=len(x))
+    path = np.array(path)
+    expected = np.array([(0, 0), (1, 1), (2, 2), (3, 3), (4, 4)])
+
+    assert d == 0
+    ok = (len(path) == len(expected)) and (path == expected).all()
+    if not ok:
+        raise RuntimeError(
+            """fastdtw is not working correctly.
+Please check the following issue:
+https://github.com/slaypni/fastdtw/issues/46
+"""
+        )
+
+
 if len(sys.argv) != 2:
     print(f"USAGE: {sys.argv[0]} config_path")
     sys.exit(-1)
+
+check_fastdtw_correctness()
 
 config = None
 with open(sys.argv[1], "r") as yml:
